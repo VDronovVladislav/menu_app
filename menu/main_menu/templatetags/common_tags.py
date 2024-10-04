27731ee14753f37ad986a3eaae2d347c41aefa6a
Vladislav <vdronovvladislav@gmail.com>
2024-10-04 15:11:59 +0300

    Настроено определение активного пункта меню

--- menu/main_menu/templatetags/common_tags.py
+++ menu/main_menu/templatetags/common_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from main_menu.models import MenuItem
+from menu.settings import PREFIX_URL
 
 register = template.Library()
 
@@ -10,6 +11,11 @@ def draw_menu(context, menu_name):
     menu_items = MenuItem.objects.filter(
         menu_name=menu_name, level=1
     ).prefetch_related('children')
+    suffix = context['request'].path
+    last_part = suffix.split('/')[-2] + '/'
+    current_url = PREFIX_URL + suffix
     return {
         "menu_items": menu_items,
+        "current_url": current_url,
+        "last_part": last_part
     }
